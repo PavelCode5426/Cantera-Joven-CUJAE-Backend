abd9fd1734b82166b5ce7ecd34447ce4e0e4620d
Laura <lauramp434@gmail.com>
2022-06-22 18:38:29 -0400

    Modificaciones en los estudiantes Agregarles el campo área

--- core/formacion_complementaria/gestionar_avales/urls.py
+++ core/formacion_complementaria/gestionar_avales/urls.py
@@ -4,7 +4,7 @@ from core.formacion_complementaria.gestionar_avales.views import ObtenerGraduado
 from .views import ObtenerCrearActualizarAval
 # Create your views here.
 app_name = 'GestionarAval'
-
+#TODO Avales no solo en Formación Complementaria
 urlpatterns = [
     path('user/<int:usuario>/aval',ObtenerCrearActualizarAval.as_view()),
     path('graduado/sin-aval',ObtenerGraduadosSinAval.as_view()),
--- custom/authentication/directorio/estudiante.py
+++ custom/authentication/directorio/estudiante.py
@@ -1,9 +1,9 @@
 __estudiantes = [
-    {'id':100,'email':'estudiante_106@gmail.com','username':'rdorsey','first_name':'Raymond','last_name':'Dorsey','direccion':'La Habana 105','anno':1},
-    {'id':101,'email':'estudiante_105@gmail.com','username':'lily10','first_name':'Lili','last_name':'Dorsey','direccion':'Ave 3ra #100','anno':3},
-    {'id':199,'email':'estudiante_104@gmail.com','username':'est01','first_name':'Rose','last_name':'Call','direccion':'La Habana 70989','anno':4},
-    {'id':182,'email':'estudiante_103@gmail.com','username':'est2010','first_name':'Loren','last_name':'Hernandez','direccion':'Ave 51 Calle 88','anno':2},
-    {'id':343,'email':'estudiante_102@gmail.com','username':'lemuspepe','first_name':'Pepe','last_name':'Lemus','direccion':'Ave 42 #4556','anno':1},
+    {'id':100,'email':'estudiante_106@gmail.com','username':'rdorsey','first_name':'Raymond','last_name':'Dorsey','direccion':'La Habana 105','anno':1, 'area':'Informatica'},
+    {'id':101,'email':'estudiante_105@gmail.com','username':'lily10','first_name':'Lili','last_name':'Dorsey','direccion':'Ave 3ra #100','anno':3, 'area':'Economia'},
+    {'id':199,'email':'estudiante_104@gmail.com','username':'est01','first_name':'Rose','last_name':'Call','direccion':'La Habana 70989','anno':4, 'area':'Mantenimiento'},
+    {'id':182,'email':'estudiante_103@gmail.com','username':'est2010','first_name':'Loren','last_name':'Hernandez','direccion':'Ave 51 Calle 88','anno':2, 'area':'Informatica'},
+    {'id':343,'email':'estudiante_102@gmail.com','username':'lemuspepe','first_name':'Pepe','last_name':'Lemus','direccion':'Ave 42 #4556','anno':1, 'area':'Informatica'},
 ]
 
 
@@ -15,6 +15,18 @@ def obtenerEstudiantes(listIDs=[]):
     from . import obtenerPorIDs
     return obtenerPorIDs(__estudiantes, listIDs)
 
+def obtenerPorAnno(__estudiantes,anno):
+    encontrado = False
+    items = list()
+    it = iter(__estudiantes)
+    while  not encontrado:
+        try:
+            elem = next(it)
+            if elem['anno'] == anno:
+                items = elem
+                encontrado = True
+        except StopIteration:
+            encontrado = True
 
-
+    return items
 
